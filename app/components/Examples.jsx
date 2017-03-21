7c73fde2a5517c4c35770f785a8307f230a5e2a1
Sagar Thapa <sagar@Sagars-MacBook-Pro.local>
2017-03-21 21:08:31 +0545

    added extra !

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -5,7 +5,8 @@ var Examples = (props) => {
   return (
     <div>
       <h2>Examples Component!</h2>
-      <p>Welcome to examples page!</p>
+      <p>Welcome to examples page!!</p>
+
     </div>
   )
 };
